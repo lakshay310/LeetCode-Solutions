2fddc35fc8adcd415bb9a90bab7749c8b04b81e7
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-07 06:52:33 +0000

    Sync LeetCode submission Runtime - 2 ms (99.49%), Memory - 44.9 MB (81.00%)

---
+++ 0496-next-greater-element-i/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st=new Stack<>();
+        int[] res=new int [10001];
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty()&& st.peek()<nums2[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()){
+                res[nums2[i]]=st.peek();
+            }
+            else{
+                res[nums2[i]]=-1;
+            }
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.length;i++){
+        nums1[i]=res[nums1[i]];}
+        return nums1;
+    }
+}
