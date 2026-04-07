6ad928c65e8ba4cafe0f676b6331d65aebe8307b
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-07 14:51:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (33.85%)

--- 1013-fibonacci-number/solution.java
+++ 1013-fibonacci-number/solution.java
@@ -1,9 +1,11 @@
 class Solution {
     public int fib(int n) {
-        int a=0, b=1,c=0;
-        if (n==1) return n;
-        for(int i=1; i<n; i++){
-            c= a+b;
+        // if(n==0||n==1) return n;
+        // return fib(n-1)+fib(n-2);
+        int a=0, b=1, c=0;
+        if(n==1) return n;
+        for(int i=1; i<n;i++){
+            c=a+b;
             a=b;b=c;
         }
         return c;
