38a6eace2306440d99fd88e56108266d46d88f8a
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-07 15:27:23 +0000

    Sync LeetCode submission Runtime - 2 ms (70.40%), Memory - 44.9 MB (21.64%)

---
+++ 0022-generate-parentheses/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List <String> res=new ArrayList<>();
+        gen("",n,n,res);
+        return res;
+    }
+    public void gen(String str, int open, int close, List<String> res){
+        if(open==0&&close==0){
+            res.add(str);
+            return;
+        }
+        if(open>0){
+            gen(str+"(",open-1,close,res);
+        }
+        if(close>open){
+            gen(str+")",open,close-1,res);
+        }
+    }
+}
