21d2ca7832ac8e3c9c8d555f3798991fa267a346
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-08 05:56:59 +0000

    Sync LeetCode submission Runtime - 1 ms (99.95%), Memory - 46.8 MB (65.80%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result=0;
-        for(int num: nums){
-            result^=num;
+        for(int i=0;i<nums.length;i++){
+            result^=nums[i];
         }
         return result;
-        
     }
 }
