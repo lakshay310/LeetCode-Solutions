0cf25071228950734e97fbbd3faddf2049e7520a
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-13 06:37:04 +0000

    Sync LeetCode submission Runtime - 1 ms (76.63%), Memory - 42.3 MB (67.27%)

--- 0202-happy-number/solution.java
+++ 0202-happy-number/solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = getSquareSum(n);
-        while (fast != 1 && slow != fast) {
-            slow = getSquareSum(slow);
-            fast = getSquareSum(getSquareSum(fast));
-        }        
-        return fast == 1;
+      int slow=n;
+      int fast=getsquare(n);
+      while(fast!=1&&slow!=fast){
+        slow=getsquare(slow);
+        fast=getsquare(getsquare(fast));
+      }  
+      return fast==1;
     }
-    private int getSquareSum(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
+    private int getsquare(int n){
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            sum+=digit*digit;
+            n/=10;
         }
         return sum;
     }
