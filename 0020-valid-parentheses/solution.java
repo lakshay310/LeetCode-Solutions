05980a6b50316080f013611f3c699c58ef8af37e
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-14 15:27:10 +0000

    Sync LeetCode submission Runtime - 4 ms (36.82%), Memory - 43.4 MB (41.52%)

--- 0020-valid-parentheses/solution.java
+++ 0020-valid-parentheses/solution.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } 
-            else {
-                if (stack.isEmpty()) return false;             
-                char top = stack.pop(); 
-                if (ch == ')' && top != '(') return false;
-                if (ch == '}' && top != '{') return false;
-                if (ch == ']' && top != '[') return false;
+        Stack <Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()) return false;
+                char top=st.pop();
+                if(ch==')'&& top!='(') return false;
+                if(ch=='}'&& top!='{') return false;
+                if(ch==']'&& top!='[') return false;
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
