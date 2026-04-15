d36cdfdee1d48b5c688bfe7226c948a53a679836
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-15 16:55:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.7 MB (69.40%)

--- 0042-trapping-rain-water/solution.java
+++ 0042-trapping-rain-water/solution.java
@@ -1,26 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-       int left=0, right=height.length-1; 
-       int leftmax=0, rightmax=0;
-       int water=0;
-       while(left<right){
-        if(height[left]<height[right]){
-            if(height[left]>=leftmax){
-                leftmax=height[left];
-            }else{
-                water+=leftmax-height[left];
+        int left=0, right=height.length-1;
+        int maxl=0,maxr=0;
+        int water=0;
+        while(left<right){
+            if(height[left]<height[right]){
+                if(maxl<=height[left]){
+                    maxl=height[left];
+                }else{
+                    water+=maxl-height[left];
+                }
+                left++;
             }
-            left++;
-        }
-        else{
-            if(height[right]>=rightmax){
-                rightmax=height[right];
-            }else{
-                water+=rightmax-height[right];
+            else{
+                if(maxr<=height[right]){
+                    maxr=height[right];
+                }else{
+                    water+=maxr-height[right];
+                }
+                right--;
             }
-            right--;
         }
-       }
-       return water;
+        return water;
     }
 }
