87dd9b1dbef37f7eee79679fc05947e1b6301e14
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-15 14:39:43 +0000

    Sync LeetCode submission Runtime - 1 ms (86.81%), Memory - 44.3 MB (39.48%)

--- 0078-subsets/solution.java
+++ 0078-subsets/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
-        ans(0,nums, new ArrayList<>(),res);
+        ans(0,nums,new ArrayList<>(),res);
         return res;
     }
-    public void ans(int index, int[]nums, List<Integer> curr, List <List<Integer>> res){
+    public void ans(int index,int[]nums,List<Integer>curr,List<List<Integer>>res){
         res.add(new ArrayList(curr));
         for(int i=index;i<nums.length;i++){
             curr.add(nums[i]);
