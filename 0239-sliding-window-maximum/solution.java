d56ea2527bc1da534a56d7504bd3a53ff045fa2d
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-17 15:15:17 +0000

    Sync LeetCode submission Runtime - 30 ms (74.56%), Memory - 147 MB (49.94%)

--- 0239-sliding-window-maximum/solution.java
+++ 0239-sliding-window-maximum/solution.java
@@ -2,21 +2,20 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> dq=new ArrayDeque<>();
         int n=nums.length;
-        int [] ans=new int[n-k+1];
+        int[] ans=new int[n-k+1];
         int idx=0;
         for(int i=0;i<n;i++){
-            if(!dq.isEmpty()&& dq.peekFirst()==i-k){
+            if(!dq.isEmpty()&&dq.peekFirst()==i-k){
                 dq.removeFirst();
             }
-            while(!dq.isEmpty()&& nums[dq.peekLast()]<nums[i]){
+            while(!dq.isEmpty()&&nums[dq.peekLast()]<nums[i]){
                 dq.removeLast();
             }
-            dq.addLast(i);
+            dq.add(i);
             if(i>=k-1){
                 ans[idx++]=nums[dq.peekFirst()];
             }
         }
         return ans;
-        
     }
 }
