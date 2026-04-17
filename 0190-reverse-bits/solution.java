cc0118801979538e0eaeaa43ec1d3058c9f6684d
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-04-17 14:48:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (96.91%)

--- 0190-reverse-bits/solution.java
+++ 0190-reverse-bits/solution.java
@@ -2,9 +2,8 @@ class Solution {
     public int reverseBits(int n) {
         int r=0;
         for(int i=0;i<32;i++){
-            int last=n&1;
             r=r<<1;
-            r=r|last;
+            r|=n&1;
             n=n>>1;
         }
         return r;
