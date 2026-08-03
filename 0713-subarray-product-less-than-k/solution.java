0b2607f1ba3bce80cec96297b52992210d980387
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-08-03 15:36:16 +0000

    Sync LeetCode submission Runtime - 2 ms (99.97%), Memory - 48.8 MB (59.01%)

---
+++ 0713-subarray-product-less-than-k/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k<=1){
+            return 0;
+        }
+        int l=0;
+        int pro=1;
+        int count=0;
+        for(int r=0;r<nums.length;r++){
+            pro*=nums[r];
+            while(pro>=k){
+                pro/=nums[l];
+                l++;
+            }
+            count+=(r-l+1);
+        }
+        return count;
+    }
+}
