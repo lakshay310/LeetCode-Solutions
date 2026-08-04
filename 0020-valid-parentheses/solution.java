821e7213e21b001a36f7a5453c4444d3150d600a
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-08-04 03:52:55 +0000

    Sync LeetCode submission Runtime - 3 ms (86.06%), Memory - 43.1 MB (73.67%)

--- 0020-valid-parentheses/solution.java
+++ 0020-valid-parentheses/solution.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack <Character> st=new Stack<>();
+        Stack<Character> st=new Stack<>();
         for(char ch:s.toCharArray()){
             if(ch=='('||ch=='{'||ch=='['){
                 st.push(ch);
-            }else{
+            }
+            else{
                 if(st.isEmpty()) return false;
                 char top=st.pop();
-                if(ch==')'&& top!='(') return false;
-                if(ch=='}'&& top!='{') return false;
-                if(ch==']'&& top!='[') return false;
+                if(ch==')'&&top!='(') return false;
+                if(ch=='}'&&top!='{') return false;
+                if(ch==']'&&top!='[') return false;
             }
         }
         return st.isEmpty();
+        
     }
 }
