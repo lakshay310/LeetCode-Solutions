4c7bcefbfc5daaa76f67cee9300c29b5652f42a0
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-08-08 16:02:56 +0530

    Time: 1 ms (100.00%) | Memory: 47.9 MB (78.17%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,17 +1,21 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
+        int xor=0;
+        for(int num:nums){
+            xor^=num;
+        }
+        int rightbit;
+        if(xor==Integer.MIN_VALUE){
+            rightbit=xor;
+        }else{
+            rightbit=xor&(-xor);
+        }
         int[] res=new int[2];
-        int n=nums.length;
-        int index=0;
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count==1){
-                res[index++]=nums[i];
+        for(int num:nums){
+            if((num&rightbit)==0){
+                res[0]^=num;
+            }else{
+                res[1]^=num;
             }
         }
         return res;
