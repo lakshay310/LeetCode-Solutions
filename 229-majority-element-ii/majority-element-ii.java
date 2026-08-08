15e3d1d3b42a0f95d8e925f1441f962ec36081ed
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-08-08 15:35:52 +0530

    Time: 2 ms (99.96%) | Memory: 52.4 MB (76.08%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,18 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res=new ArrayList<>();
-        int n=nums.length;
-        int thres=n/3;
-        for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+        if(nums==null||nums.length==0){
+            return res;
+        }
+        int a=0, c1=0;
+        int b=1, c2=0;
+        for(int num:nums){
+            if(num==a){
+                c1++;
+            }else if(num==b){
+                c2++;
+            }else if(c1==0){
+                a=num;
+                c1=1;
+            }else if(c2==0){
+                b=num;
+                c2=1;
+            }else{
+                c1--;
+                c2--;
             }
-            if(count>thres&&!res.contains(nums[i])){
-                res.add(nums[i]);
+        }
+        c1=0;
+        c2=0;
+        for(int num:nums){
+            if(num==a){
+                c1++;
+            }else if(num==b){
+                c2++;
             }
+        }
+        int thres=nums.length/3;
+        if(c1>thres){
+            res.add(a);
+        }
+        if(c2>thres){
+            res.add(b);
         }
         return res;
     }
