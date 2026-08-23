10c3e8405516b9bab3bda5dbbdbfde9f5189a2b9
Lakshay Jain <jainlakshay.jain31@gmail.com>
2026-08-23 16:29:56 +0530

    Time: 4 ms (100.00%) | Memory: 46 MB (53.10%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0||(x%10==0&&x!=0)) return false;
+        int reverseHalf=0;
+        while(x>reverseHalf){
+            int d=x%10;
+            reverseHalf=reverseHalf*10+d;
+            x/=10;
+        }
+        return x==reverseHalf||x==reverseHalf/10;
+    }
+}
